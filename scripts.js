ef0a1dc6a0663ff88168338a542e8ac9d648783f
Joel Wiesner <j.wiesner88@gmail.com>
2023-02-20 12:28:52 -0800

    added JS to score
    allowed score to go up and down upon button clicks

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-console.log("hellow World")
+const increment = document.getElementById('score-up')
+const decrement = document.getElementById('score-down')
+const scoreDisplay = document.getElementById('score')
+
+let score = 0
+
+increment.addEventListener("click", function(){
+    score++
+    scoreDisplay.innerHTML = score
+    decrement.disabled = false
+    decrement.style.opacity = "1"
+   
+})
+
+decrement.addEventListener("click", function(){
+    score--
+    scoreDisplay.innerHTML = score
+    if (score === 0) {
+        decrement.style.opacity = "0.5"
+    }
+})
